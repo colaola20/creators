5c09350b793c66b5aec6c13563321713613e7ac0
colaola20 <soryo@farmingdale.edu>
2026-01-27 21:30:37 -0500

    replace API with ANON in env variable naming.

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
 const URL = import.meta.env.VITE_SUPABASE_URL;
-const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, API_KEY);
